c3272ffda6e39a8c8758ab271bd91c73a1cb298f
kristina <paskoveckristina3@gmail.com>
2023-11-23 18:47:51 +0300

    1.2. Входные данные в тесте [#297750]

---
+++ src/test/java/ru/job4j/condition/X2Test.java
@@ -0,0 +1,63 @@
+package ru.job4j.condition;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.*;
+
+class X2Test {
+
+    @Test
+    void whenA10B0C0X2Then40() {
+        int a = 10;
+        int b = 0;
+        int c = 0;
+        int x = 2;
+        int expected = 40;
+        int rsl = X2.calc(10, 0, 0, 2);
+        assertThat(expected).isEqualTo(rsl);
+    }
+
+    @Test
+    void whenA1B1C1X1Then3() {
+        int a = 1;
+        int b = 1;
+        int c = 1;
+        int x = 1;
+        int expected = 3;
+        int rsl = X2.calc(1, 1, 1, 1);
+        assertThat(expected).isEqualTo(rsl);
+    }
+
+    @Test
+    void whenA0B1C1X1Then2() {
+        int a = 0;
+        int b = 1;
+        int c = 1;
+        int x = 1;
+        int expected = 2;
+        int rsl = X2.calc(0, 1, 1, 1);
+        assertThat(expected).isEqualTo(rsl);
+    }
+
+    @Test
+    void whenA1B1C0X1Then2() {
+        int a = 1;
+        int b = 1;
+        int c = 0;
+        int x = 1;
+        int expected = 2;
+        int rsl = X2.calc(1, 1, 0, 1);
+        assertThat(expected).isEqualTo(rsl);
+    }
+
+    @Test
+    void whenA1B1C1X0Then1() {
+        int a = 1;
+        int b = 1;
+        int c = 1;
+        int x = 0;
+        int expected = 1;
+        int rsl = X2.calc(1, 1, 1, 0);
+        assertThat(expected).isEqualTo(rsl);
+    }
+}
